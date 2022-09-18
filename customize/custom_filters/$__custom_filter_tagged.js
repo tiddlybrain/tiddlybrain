1a8486285c9ccb687a9c5d957d229176c8ede200
tiddlybrain <tiddlybrain@yuz.me>
2022-09-18 13:54:10 +0200

    New feature: tagged filter supports invert

--- customize/custom_filters/$__custom_filter_tagged.js
+++ customize/custom_filters/$__custom_filter_tagged.js
@@ -17,18 +17,21 @@ Filter input tiddlers in which all tags specified in the operand are tagged.
 Export our filter function
 */
 exports.tagged = function(source,operator,options) {
-    var results = [],
-        tagsArray = $tw.utils.parseStringArray(operator.operand);
-
-    source(function(tiddler,title) {
-        if(tiddler && tiddler.fields.tags && tagsArray.every(function(tag) {
-            return tiddler.fields.tags.indexOf(tag) != -1;
-        })) {
-            results.push(title);
-        };
-    });
-
-    return results;
+	var results = [], invert = operator.prefix === "!";
+	var tagsArray = $tw.utils.parseStringArray(operator.operand);
+	source(function(tiddler,title) {
+		if(tiddler && tiddler.fields.tags) {
+			let result = tagsArray.every(function(tag) {
+				return tiddler.fields.tags.indexOf(tag) !== -1;
+			});
+			if(invert) {
+				if(result === false) results.push(title);
+			} else {
+				if(result === true) results.push(title);
+			}
+		}
+	});
+	return results;
 };
 
 })();
