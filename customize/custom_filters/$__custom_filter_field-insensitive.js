82d7f9fedfc669103eae4c93a4d4049de2292c88
tiddlybrain <tiddlybrain@yuz.me>
2022-11-06 17:14:56 +0100

    update topic search algo

---
+++ customize/custom_filters/$__custom_filter_field-insensitive.js
@@ -0,0 +1,45 @@
+/*\
+title: $:/custom/filter/field-insensitive.js
+type: application/javascript
+tags: customized
+module-type: filteroperator
+
+Filter operator for comparing fields for equality (case insensitive version)
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Export our filter function
+*/
+exports.fieldInsensitive = function(source,operator,options) {
+	var results = [], fieldname = operator.suffix || operator.operator || "title";
+	if(operator.prefix === "!") {
+		source(function(tiddler,title) {
+			if(tiddler) {
+				var text = tiddler.getFieldString(fieldname);
+				if(text !== null && text.toLowerCase() !== operator.operand.toLowerCase()) {
+					results.push(title);
+				}
+			} else {
+				results.push(title);
+			}
+		});
+	} else {
+		source(function(tiddler,title) {
+			if(tiddler) {
+				var text = tiddler.getFieldString(fieldname);
+				if(text !== null && text.toLowerCase() === operator.operand.toLowerCase()) {
+					results.push(title);
+				}
+			}
+		});
+	}
+	return results;
+};
+
+})();
