11c8c85bf60508549317962039e4f3592ab915e3
Zac YU <git@yuz.me>
2022-11-22 12:49:56 +0100

    hide empty indexes for data-table

--- customize/core_modules/$__core_modules_filters_indexes.js
+++ customize/core_modules/$__core_modules_filters_indexes.js
@@ -19,14 +19,20 @@ Export our filter function
 exports.indexes = function(source,operator,options) {
 	var results = [], opt = operator.suffix;
 	source(function(tiddler,title) {
-		var data = options.wiki.getTiddlerDataCached(title);
-		if(data) {
-			if (opt === "non-empty") {
-				Object.keys(data).forEach(key => {
-					if (data[key].length !== 0) results.push(key);
-				});
-			} else {
-				$tw.utils.pushTop(results,Object.keys(data));
+		if(tiddler) {
+			var data = options.wiki.getTiddlerDataCached(title);
+			if(data) {
+				if (opt === "non-empty") {
+					var k = [], celltpl = tiddler.getFieldString("celltpl");
+					if(options.wiki.tiddlerExists(celltpl)) {
+						k = Object.keys(options.wiki.getTiddlerDataCached(celltpl) || {});
+					}
+					Object.keys(data).forEach(key => {
+						if (data[key].length !== 0 || k.indexOf(key) !== -1) results.push(key);
+					});
+				} else {
+					$tw.utils.pushTop(results,Object.keys(data));
+				}
 			}
 		}
 	});
