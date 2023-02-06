56164441ac30c75392222aa67e9b04ec26ed97bc
tiddlybrain <tiddlybrain@yuz.me>
2023-02-06 21:31:48 +0100

    Add new filter: hasTiddler

--- customize/custom_filters/$__custom_filter_getAllTiddlers.js
+++ customize/custom_filters/$__custom_filter_getAllTiddlers.js
@@ -5,6 +5,7 @@ tags: customized
 module-type: filteroperator
 
 1. Find all tiddlers inside of normal tiddlers, e.g. [[tiddlers]getAllTiddlers:note[abc]]
+2. Return tiddlers that have specific tiddler in its content, e.g. [[tiddlers]hasTiddler[abc]]
 
 \*/
 (function(){
@@ -35,6 +36,36 @@ exports.getAllTiddlers = function(source,operator,options) {
 						}
 					}
 				});
+			}
+		}
+	});
+	return results;
+};
+
+exports.hasTiddler = function(source,operator,options) {
+	var results = [], matches, pattern = /<<l\s+'(.+?)'.*>>|<<l\s+"(.+?)".*>>|\[\[.+\|(.+)\]\]|\[\[(.+)\]\]/g, value = operator.operand;
+	source(function(tiddler,title) {
+		if(tiddler) {
+			if(options.wiki.tiddlerExists(value)) {
+				let content = tiddler.getFieldString("caption") + tiddler.getFieldString("description") + tiddler.getFieldString("text");
+				while(matches = pattern.exec(content)) {
+					let result = undefined;
+					matches.shift();
+					matches.every(match => {
+						if(match === value) {
+							result = value;
+							return false;
+						} else {
+							return true;
+						}
+					});
+					if(result === value) {
+						results.push(title);
+						break;
+					}
+				}
+			} else {
+				results.push(title);
 			}
 		}
 	});
