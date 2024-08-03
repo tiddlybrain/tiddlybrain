696e2ebc447ec2c449a17e518fc07e9e696eb8a8
Zac YU <git@yuz.me>
2024-08-03 13:43:13 +0200

    Added new text operation: insert-link, updated editor toolbar buttons: link & link-dropdown to use insert-link

---
+++ customize/custom_modules/$__custom_module_editor_operations_text_insert-link.js
@@ -0,0 +1,32 @@
+/*\
+title: $:/custom/module/editor/operations/text/insert-link.js
+type: application/javascript
+tags: customized
+module-type: texteditoroperation
+
+Text editor operation to insert a link
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports["insert-link"] = function(event,operation) {
+	var editTiddler = this.wiki.getTiddler(this.editTitle), editTiddlerType = editTiddler.fields.type;
+	if(editTiddlerType === "application/x-tiddler-dictionary") {
+		operation.replacement = event.paramObject.title;
+	} else {
+		let tiddler = this.wiki.getTiddler(event.paramObject.title),
+			title = event.paramObject.title,
+			caption = operation.selection || tiddler.fields.caption;
+		operation.replacement = `<<l "${title}" "${caption}">>`;
+	}
+	operation.cutStart = operation.selStart;
+	operation.cutEnd = operation.selEnd;
+	operation.newSelStart = operation.selStart;
+	operation.newSelEnd = operation.selStart + operation.replacement.length;
+};
+
+})();
