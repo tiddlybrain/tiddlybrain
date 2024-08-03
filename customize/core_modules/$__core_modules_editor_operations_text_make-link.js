ced9ed74bf6af67be97f288cb2ed10537fa47c00
Zac YU <git@yuz.me>
2024-08-03 10:16:43 +0200

    Updated Editor Toolbar Button: link

---
+++ customize/core_modules/$__core_modules_editor_operations_text_make-link.js
@@ -0,0 +1,40 @@
+/*\
+title: $:/core/modules/editor/operations/text/make-link.js
+type: application/javascript
+tags: customized
+module-type: texteditoroperation
+
+Text editor operation to make a link
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports["make-link"] = function(event,operation) {
+	var pattern = /^<<l\s+["'](.+?)["']/, re = new RegExp(pattern), title, is_link;
+	if(re.test(operation.selection)) {
+		title = operation.selection.match(pattern)[1];
+		is_link = true;
+	} else {
+		title = operation.selection;
+		is_link = false;
+	}
+	var tiddler = this.wiki.getTiddler(title);
+	if(tiddler) {
+		if(is_link) {
+			operation.replacement = title;
+		} else {
+			let caption = tiddler.fields.caption;
+			operation.replacement = `<<l "${title}" "${caption}">>`;
+		}
+		operation.cutStart = operation.selStart;
+		operation.cutEnd = operation.selEnd;
+		operation.newSelStart = operation.selStart;
+		operation.newSelEnd = operation.selStart + operation.replacement.length;
+	}
+};
+
+})();
