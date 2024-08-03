73a89a4341733ffbc48e9abaacefb4557c96e37e
Zac YU <git@yuz.me>
2024-08-03 13:46:13 +0200

    Updated text operation: excise, updated editor toolbar buttons: excise&excise-dropdown to use the new excise operation

---
+++ customize/core_modules/$__core_modules_editor_operations_text_excise.js
@@ -0,0 +1,31 @@
+/*\
+title: $:/core/modules/editor/operations/text/excise.js
+type: application/javascript
+tags: customized
+module-type: texteditoroperation
+
+Text editor operation to excise the selection to a new tiddler
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports["excise"] = function(event,operation) {
+	var editTiddler = this.wiki.getTiddler(this.editTitle), editTiddlerType = editTiddler.fields.type;
+	var excisionTitle = event.paramObject.title, caption = event.paramObject.caption, field = "text";
+	this.wiki.setText(excisionTitle, field, null, operation.text.substring(operation.selStart,operation.selEnd));
+	if(editTiddlerType === "application/x-tiddler-dictionary") {
+		operation.replacement = excisionTitle;
+	} else {
+		operation.replacement = `<<l "${excisionTitle}" "${caption}">>`;
+	}
+	operation.cutStart = operation.selStart;
+	operation.cutEnd = operation.selEnd;
+	operation.newSelStart = operation.selStart;
+	operation.newSelEnd = operation.selStart + operation.replacement.length;
+};
+
+})();
