67f41d153abab86e58495eca9cfd850eb8dba54f
Zac YU <git@yuz.me>
2024-08-10 17:25:54 +0200

    updated: colorize and Roles manager control panel

--- customize/custom_filters/$__custom_filter_colorize.js
+++ customize/custom_filters/$__custom_filter_colorize.js
@@ -17,26 +17,27 @@ Turn tiddler titles into Hex colors
 Helper functions
 */
 //https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
-var stringToColor = function(str) {
+var stringToColor = function(str,opacity) {
 	var hash = 0;
 	for (var i = 0; i < str.length; i++) {
 		hash = str.charCodeAt(i) + ((hash << 5) - hash);
 	}
 	var color = '#';
 	for (var i = 0; i < 3; i++) {
-		var value = (hash >> (i * 8)) & 0xFF;
-		color += ('00' + value.toString(16)).substr(-2);
+		const value = (hash >> (i * 8)) & 0xFF; // Get the byte value
+		const hex = value.toString(16).padStart(2, '0'); // Convert to hex and ensure 2 digits
+		color += hex; // Append to the color string
 	}
-	return color;
+	return color + opacity;
 }
 
 /*
 Export our filter function
 */
 exports.colorize = function(source,operator,options) {
-	var results = [];
+	var results = [], opacity = operator.operand || "ff";
 	source(function(tiddler,title) {
-		var colorCode = stringToColor(title);
+		var colorCode = stringToColor(title,opacity);
 		results.push(colorCode);
 	});
 	return results;
